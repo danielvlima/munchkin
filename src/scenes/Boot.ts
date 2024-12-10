4f212c277ad4e3ae30644437217d61eba37022e9
Daniel Lima <danielvlima98@gmail.com>
2024-12-10 03:31:46 -0300

    mudando imagens de template

--- src/scenes/Boot.ts
+++ src/scenes/Boot.ts
@@ -9,8 +9,9 @@ export class Boot extends Scene {
         //  The Boot Scene is typically used to load in any assets you require for your Preloader, such as a game logo or background.
         //  The smaller the file size of the assets, the better, as the Boot Scene itself has no preloader.
 
-        this.load.image('background', 'assets/bg.gif');
-        this.load.image('game-backbround', 'assets/game-bg.jpg');
+        this.load.image('background', 'assets/bg-menu.png');
+        this.load.image('game-backbround', 'assets/game-bg.png');
+        this.load.image('door-card-back', 'assets/door-card-back.png');
     }
 
     create() {
--- src/scenes/MainMenu.ts
+++ src/scenes/MainMenu.ts
@@ -9,26 +9,34 @@ export class MainMenu extends Scene {
     }
 
     create() {
-        this.background = this.add.image(512, 384, 'background').setScale(0.5);
+        this.background = this.add.image(512, 384, 'background').setScale(0.8);
 
-        this.logo = this.add.image(512, 300, 'logo').setScale(0.4);
+        this.logo = this.add.image(512, 300, 'logo').setScale(0.8);
         //quero um branco menos claro
         // this.logo.setTintFill(0xFFFFFF);
-        this.logo.setTintFill(0xa6aa3e);
+        // this.logo.setTintFill(0xa6aa3e);
+
+        //botao branco
+        this.startButton = this.add.text(512, 430, 'Iniciar Jogo', {
+            fontSize: '42px',
+            color: '#ffffff',
+            fontStyle: 'bold',
+            padding: {
+                x: 20,
+                y: 10
+            },
+            resolution: 2,
+            fontFamily: 
 
 
-        this.startButton = this.add.text(512, 460, 'Iniciar Jogo', {
-            fontFamily: 'monospace', fontSize: 40, color: '#a6aa3e',
-            align: 'center', resolution: 1.5,
         }).setOrigin(0.5).setInteractive();
+
         this.startButton.on('pointerdown', () => {
 
             this.scene.start('Game');
 
         });
-        // this.input.on('gameobjectdown', () => {
-        //     this.scene.start('Game');
-        // });
+
     }
     update() {
 
